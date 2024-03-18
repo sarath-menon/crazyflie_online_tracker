6a9f9b40546791d914c31b5b83f73d5e23cf6fbc
sarath-menon <sarathmenon.downey@gmail.com>
2024-03-18 11:50:55 +0100

    added launch files to path

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'crazyflie_online_tracker'
+
+
 
 setup(
     name=package_name,
@@ -10,6 +14,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
