da307e46f3a9efed402faaaf4d025c505770e419
sarath-menon <sarathmenon.downey@gmail.com>
2024-05-01 16:02:56 +0200

    added param

--- launch/default_controller.py
+++ launch/default_controller.py
@@ -4,13 +4,14 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch.actions import LogInfo, RegisterEventHandler
 from launch.event_handlers import OnProcessStart
+import time
 
 def generate_launch_description():
 
     # sim_node = Node(
     #     package='crazyflie_online_tracker',
     #     executable='linear_simulator',
-    #     name='linear_simulator',
+    #     name='linear_simulator',003] [controller_default]: Trajectory data has been saved to/home/sarath/drones/catkin_ws/src/crazyflie_online_tracker/data/20240501135751_RLS_circular_target_T50_f50_mode0.npz
     # ) 
 
     controller_node =  Node(
@@ -29,6 +30,9 @@ def generate_launch_description():
             package='crazyflie_online_tracker',
             executable='state_estimator_target_virtual',
             name='state_estimator_target_virtual',
+            parameters=[{
+                'wait_for_drone_ready': LaunchConfiguration('wait_for_drone_ready')
+            }]
     )
 
     
@@ -54,6 +58,11 @@ def generate_launch_description():
             'filename',
             default_value='RLS',
             description='Filename'
+        ),
+        DeclareLaunchArgument(
+            'wait_for_drone_ready',
+            default_value='False',
+            description='Wait for drone ready'
         ),
         
        controller_node, 
